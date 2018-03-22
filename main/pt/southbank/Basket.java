ba8594506669cb1b484d1d299eeb95ceeba659cf
Carlos Pedro <Carlos.Pedro@immform.org.uk>
2018-03-22 10:56:17 +0000

    Add products to basket

--- main/pt/southbank/Basket.java
+++ main/pt/southbank/Basket.java
@@ -1,5 +1,23 @@
 package pt.southbank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+	private List<String> items;
+
+	public Basket() {
+		this.items = new ArrayList<>();
+	}	
+	
+	
+	public void add(String product) {
+		items.add(product);
+		
+	}
+
+	public List<String> get() {				
+		return items;
+	}
 
 }
--- tests/pt/southbank/BasketTests.java
+++ tests/pt/southbank/BasketTests.java
@@ -1,5 +1,22 @@
 package pt.southbank;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+
+
 public class BasketTests {
 
+	@Test public void testAddItemToBasket() {
+		Basket basket = new Basket();
+		String product = "butter";
+		basket.add(product);
+		
+		List<String> productsInBasket =  basket.get();
+		assertEquals( 1, productsInBasket.size());
+		assertEquals(product, productsInBasket.get(0));
+	}
 }
